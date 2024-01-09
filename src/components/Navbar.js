4c810a038406ab577ef8c1cd109cd122bf1d18a1
RAJVEER SINGH <syrajveer639@gmail.com>
2024-01-10 01:16:46 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CiCircleQuestion } from "react-icons/ci";
 
 const Navbar = (props) => {
   return (
-    <div className='flex justify-between items-center ml-52 mr-10 bg-[#FAFAFA] max-w-[2860px] px-16 py-4 mx-auto mt-4 mb-8 text-white'>
+    <div className='flex justify-between items-center ml-40 mr-10 bg-[#FFFFFF] max-w-[2860px] px-16 py-4 mx-auto mt-4 mb-8 text-white'>
         
 
       {/* Payments */}
       <Link to="/">
-        <p className=' text-3xl text-[#29272c]'>Payments</p>
+        <p className=' text-2xl w-[70px] h-[22px] text-inter text-[#29272c]'>Payments</p>
       </Link>
-
+      <div className='flex mr-[490px] mt-[15px]'>
+      <CiCircleQuestion className='text-[black] text-[12px] mr-[6px] mt-[4px]'/>
+      <div className='text-[black] font-inter text-[12px]'>How it works</div>
+      </div>
       {/* Search Bar */}
       <div>
         {/* Add your search bar component or input here */}
-        <input type="text" placeholder="Search features, tutorials, etc." className='border bg-[#F2F2F2] px-2 py-1 rounded-md w-[600px] h-[40px]'/>
+        <input type="text" placeholder="Search features, tutorials, etc." className='border bg-[#F2F2F2] px-2 py-1 rounded-md w-[450px] h-[40px]'/>
       </div>
-
-      {/* Navigation Links */}
-      <nav>
-        <ul className='text-richblack-100 flex gap-x-6 font-bold '>
-          <li>
-            <Link to="/" className='hover:bg-white hover:text-richblack-900'></Link>
-          </li>
-        </ul>
-      </nav>
 
       
 
