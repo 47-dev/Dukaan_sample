4d3fb060730144f062d52287a7fe6ec843fc07a6
RAJVEER SINGH <syrajveer639@gmail.com>
2024-01-10 01:27:59 +0530

    Third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { CiCircleQuestion } from "react-icons/ci";
+import { RiMessage3Fill } from "react-icons/ri";
+import { MdArrowDropDown } from "react-icons/md";
 
 const Navbar = (props) => {
   return (
@@ -11,7 +13,7 @@ const Navbar = (props) => {
       <Link to="/">
         <p className=' text-2xl w-[70px] h-[22px] text-inter text-[#29272c]'>Payments</p>
       </Link>
-      <div className='flex mr-[490px] mt-[15px]'>
+      <div className='flex mr-[340px] mt-[15px]'>
       <CiCircleQuestion className='text-[black] text-[12px] mr-[6px] mt-[4px]'/>
       <div className='text-[black] font-inter text-[12px]'>How it works</div>
       </div>
@@ -20,6 +22,9 @@ const Navbar = (props) => {
         {/* Add your search bar component or input here */}
         <input type="text" placeholder="Search features, tutorials, etc." className='border bg-[#F2F2F2] px-2 py-1 rounded-md w-[450px] h-[40px]'/>
       </div>
+
+      <RiMessage3Fill className='text-[#4c4c4c] text-xl bg-[#e6e6e6] rounded-xl pt[10px] w-[50px] h-[30px]'/>
+      <MdArrowDropDown className='text-[#4c4c4c] text-xl bg-[#e6e6e6] rounded-xl pt[10px] w-[50px] h-[30px]'/>
 
       
 
